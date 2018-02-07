2057709ba414a2e24dc9d8adf4784350b28f0342
aarav kumar <aaravkumar96.ak@gmail.com>
2018-02-07 16:13:16 +0530

    setup app for heroku version2

--- seed/product-seeder.js
+++ seed/product-seeder.js
@@ -3,7 +3,7 @@ var Product = require('../models/product');
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost:27017/shop');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/shop');
 
 var products =[
     new Product({
